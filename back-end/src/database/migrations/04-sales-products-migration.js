2c10716ad99b68411ea50f35072e2f76261034b3
HenriqueSerres <henriqueserres@gmail.com>
2022-08-04 18:46:32 -0300

    feat(04-sales-products-migration.js): migration tabela de sales_products criada

---
+++ back-end/src/database/migrations/04-sales-products-migration.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Sales_Products', {
+      sale_id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Sales',
+          key: 'id'
+        }
+      },
+      product_id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Products',
+          key: 'id'
+        }
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Sales_Products');
+  }
+};
