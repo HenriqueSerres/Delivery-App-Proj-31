55e72819ffd036ed075b2b792da22256bad0b878
HenriqueSerres <henriqueserres@gmail.com>
2022-08-04 19:39:15 -0300

    feat(productsModel.js): Model da tabela products criada

---
+++ back-end/src/database/models/productsModel.js
@@ -0,0 +1,28 @@
+const Prducts = (sequelize, DataTypes) => {
+	const Prducts = sequelize.define('Prducts', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    price: {
+      allowNull: false,
+      type: DataTypes.FLOAT
+    },
+    url_image: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+	}, {
+		timestamps: false,
+	});
+
+	return Prducts;
+};
+
+module.exports = Prducts;
