dca47c23486b35c46041136d75b142ad02f09460
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-05 17:58:34 -0300

    style(01-user-migration.js): :art: Alterações feitas para corrigir erros de lint.

--- back-end/src/database/migrations/01-user-migration.js
+++ back-end/src/database/migrations/01-user-migration.js
@@ -6,26 +6,27 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       email: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       password: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       role: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
     });
   },
-  down: async (queryInterface, Sequelize) => {
+
+  down: async (queryInterface, _Sequelize) => {
     await queryInterface.dropTable('Users');
   }
 };
