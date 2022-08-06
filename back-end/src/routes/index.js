4b2f73b7bb1b45350d1035bcbf2c20091712c17c
HenriqueSerres <henriqueserres@gmail.com>
2022-08-06 17:21:56 -0300

    feat(index.js): Add login route

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const mockRoutes = require('./mockRoutes');
+const loginRoutes = require('./login');
 
 const routes = Router();
 
 routes.use('/mock', mockRoutes);
+routes.use('/login', loginRoutes);
 
 module.exports = routes;
