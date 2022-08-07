09cc3aefc29ba317d4fb0d536520711bb44dafc9
HenriqueSerres <henriqueserres@gmail.com>
2022-08-07 19:00:25 -0300

    fix: movendo fora database

--- back-end/src/database/middlewares/errorMiddleware.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  errorMiddleware: (error, _req, res, _next) => {
-    if (error.status) {
-      return res.status(error.status).json({ message: error.message });
-    }
-
-    return res.status(500).json({ message: error.message });
-  },
-};
