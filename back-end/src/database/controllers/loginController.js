36476dc9aaefd2744e02bc66c865bae8954958c3
HenriqueSerres <henriqueserres@gmail.com>
2022-08-07 18:59:18 -0300

    feat(loginController.js): Alterando importações

--- back-end/src/database/controllers/loginController.js
+++ back-end/src/database/controllers/loginController.js
@@ -2,7 +2,7 @@ const loginService = require('../services/loginService');
 
 const loginUser = async (req, res, next) => {
   try {
-    const { email, password } = req.body;
+    const { email, password } = req.body;    
     const result = await loginService.loginUser(email, password);
     return res.status(200).json(result);
   } catch (error) {
