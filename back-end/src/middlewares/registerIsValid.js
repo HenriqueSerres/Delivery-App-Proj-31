ec14e9615396d13e359521e38b7df8854439a5c1
HenriqueSerres <henriqueserres@gmail.com>
2022-08-07 19:29:15 -0300

    fix(registerIsValid.js): Fix numero de caracteres para registro de usuario

--- back-end/src/middlewares/registerIsValid.js
+++ back-end/src/middlewares/registerIsValid.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 const REGISTER = Joi.object({
-  name: Joi.string().min(3).required(),
+  name: Joi.string().min(12).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
   role: Joi.string().required(),
