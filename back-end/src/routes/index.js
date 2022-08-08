e2e4c19db2415f92531292bc0eea36478350fca1
caio-cmc <caiocesarmcruz@gmail.com>
2022-08-08 18:55:29 -0300

    feat(routes/index.js): Seller route Implemented

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 const mockRoutes = require('./mockRoutes');
 const loginRoutes = require('./login');
 const registerRoutes = require('./register');
+const sellerRoutes = require('./seller');
 
 const routes = Router();
 
 routes.use('/mock', mockRoutes);
 routes.use('/login', loginRoutes);
 routes.use('/register', registerRoutes);
+routes.use('/seller', sellerRoutes);
 
 module.exports = routes;
