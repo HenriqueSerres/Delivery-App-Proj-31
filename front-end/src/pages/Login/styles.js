e3711f4626af7ef7ea41b85aa033fb7f977fc95e
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-08-08 16:49:17 -0300

    feat(Login/styles.js): Estilizando Login

---
+++ front-end/src/pages/Login/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > form {
+    height: 60vh;
+    width: 60vw;
+    
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+
+    border: 1px solid black;
+  }
+`;
