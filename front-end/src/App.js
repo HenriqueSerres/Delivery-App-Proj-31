c6629184546b7231988926f10ad9db57de28c7e3
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-08-08 17:05:40 -0300

    feat(frontend): alteração no react-router-dom, devido a incompatibilidade com o linter

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import PathRouter from './PathRouter';
+import ContextProvider from './context/ContextProvider';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <ContextProvider>
+      <PathRouter />
+    </ContextProvider>
   );
 }
 
