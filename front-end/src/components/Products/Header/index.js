ceab0126c0f623379766dc5c9babc45e6bad772e
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-08-08 17:00:51 -0300

    feat(Products/Header/index.js): Criando a estrutura e a lógica do componente Header da página Products

---
+++ front-end/src/components/Products/Header/index.js
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import Context from '../../../context/Context';
+
+import { Container } from './styles';
+
+function Header() {
+  const { name } = useContext(Context);
+  return (
+    <Container>
+      {/* Header */}
+      {/* Produtos */}
+      <h3 data-testid="customer_products__element-navbar-link-products">Produtos</h3>
+      {/* Meus Pedidos */}
+      <h3 data-testid="customer_products__element-navbar-link-orders">Meus Pedidos</h3>
+      {/* Usuário */}
+      <h3 data-testid="customer_products__element-navbar-user-full-name">{name}</h3>
+      {/* Sair */}
+      <button
+        data-testid="customer_products__element-navbar-link-logout"
+        type="button"
+        onClick={ () => navigate('/') }
+      >
+        sair
+      </button>
+    </Container>
+  );
+}
+
+export default Header;
