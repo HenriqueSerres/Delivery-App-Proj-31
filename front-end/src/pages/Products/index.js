8604caa33fe05b672dbec668efb0ec2011848e78
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-08-08 16:48:27 -0300

    feat(Products/index.js): Criando a estrutura e a lógica de Products

---
+++ front-end/src/pages/Products/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../../components/Products/Header';
+
+import { Container } from './styles';
+
+function Products() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
+
+export default Products;
