c33e4098af4b1a8723fd0b28a2bff550e37d9eab
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-08-08 16:41:32 -0300

    feat(PathRouter/index.js): implementação das rotas Login, Register, Products

---
+++ front-end/src/PathRouter/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Products from '../pages/Products';
+
+function PathRouter() {
+  return (
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="/customer/products" component={Products} />
+      <Route exact path="/" component={Login} />
+    </Switch>
+  );
+}
+
+export default PathRouter;
