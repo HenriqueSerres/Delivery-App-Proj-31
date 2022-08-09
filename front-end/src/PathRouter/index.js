488b255272ff1f2321a2a7f31dab7494bec0e2ec
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-08-09 17:10:30 -0300

    feat: PathRouter implementando rota de ShoppingCart

--- front-end/src/PathRouter/index.js
+++ front-end/src/PathRouter/index.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Products from '../pages/Products';
+import ShoppingCart from '../pages/ShoppingCart';
 
 function PathRouter() {
   return (
@@ -11,6 +12,7 @@ function PathRouter() {
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
       <Route path="/customer/products" component={Products} />
+      <Route path="/customer/checkout" component={ShoppingCart} />
       <Route exact path="/" component={Login} />
     </Switch>
   );
