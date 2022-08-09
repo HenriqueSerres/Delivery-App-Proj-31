4b7a62cfd2517bc70d216ae6c2c543771a1d5c20
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-08-09 17:14:05 -0300

    docs(pages/Products): Estilizando a página

--- front-end/src/pages/Products/styles.js
+++ front-end/src/pages/Products/styles.js
@@ -1,3 +1,17 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const BoxProducts = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-around;
+`;
