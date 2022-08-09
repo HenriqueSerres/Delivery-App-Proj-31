8cb5ae085412e11cbc8ebb291ca9968e1b04ac56
HenriqueSerres <henriqueserres@gmail.com>
2022-08-09 20:17:18 -0300

    feat(salesController.js): Add post orders and get sallers name

--- back-end/src/controllers/salesController.js
+++ back-end/src/controllers/salesController.js
@@ -1,11 +1,36 @@
 const salesService = require('../services/salesService');
 
-const findOrders = async (req, res) => {
-  const { id, role } = req.user.data;
-  const sales = await salesService.filterAllOrders(id, role);
+const findOrders = async (req, res, next) => {
+  try {
+    const { id, role } = req.user.data;
+    const sales = await salesService.filterAllOrders(id, role);
   return res.status(200).json(sales);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const addNewOrder = async (req, res, next) => {
+  try {
+    const { id, role } = req.user.data;
+    const newOrder = await salesService.addNewOrder(id, role, req.body);
+  return res.status(200).json(newOrder);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getAllSallers = async (req, res, next) => {
+  try {
+    const sallers = await salesService.getAllSallers();
+    return res.status(200).json(sallers);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
   findOrders,
+  addNewOrder,
+  getAllSallers,
 };
