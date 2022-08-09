aed5ac9c88f383e308a5ccc901da60dcacf2679d
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-08-09 17:12:05 -0300

    feat(pages/ShoppingCart): Criando a estrutura e a lógica de ShoppingCart

--- front-end/src/pages/ShoppingCart/index.js
+++ front-end/src/pages/ShoppingCart/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Cart from '../../components/ShoppingCart/Cart';
 
 import { Container } from './styles';
 
 function ShoppingCart() {
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    const getCartFromLocalStorage = async () => {
+      const cartItemsData = await localStorage.getItem('cart');
+      setCartItems(JSON.parse(cartItemsData));
+    };
+    getCartFromLocalStorage();
+  }, []);
+
+  console.log(cartItems);
+
   return (
     <Container>
-      <h1>a</h1>
+      {cartItems !== null &&
+        cartItems !== undefined &&
+        cartItems.length > 0 &&
+        cartItems.filter((item) => item.quantity > 0).map((item) => <Cart item={item} />)}
     </Container>
   );
 }
