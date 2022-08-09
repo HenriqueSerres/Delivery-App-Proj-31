e8f17631c9bf0002e37056443e33d7a2539b7a5c
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-09 17:13:07 -0300

    feat(components/OrderCard.jsx): Validação de props implementada e formatção de código corrigido

--- front-end/src/components/OrderCard.jsx
+++ front-end/src/components/OrderCard.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const OrderCard = ({
+function OrderCard({
   order,
   status,
   orderDate,
   totalPrice,
   address,
   dataTestIdOrderId,
-}) => {
-  return(
+}) {
+  return (
     <div className="div-card">
       <div className="div-card-order">
         <span
           className="span-card-order"
-          data-testid={`seller_orders__element-order-id-${dataTestIdOrderId}`}
+          data-testid={ `seller_orders__element-order-id-${dataTestIdOrderId}` }
         >
           { order }
         </span>
@@ -22,33 +23,48 @@ const OrderCard = ({
         <div className="div-card-info-1">
           <div className="div-card-info-1-order-status">
             <span
-              data-testid={`seller_orders__element-delivery-status-${dataTestIdOrderId}`}
+              data-testid={
+                `seller_orders__element-delivery-status-${dataTestIdOrderId}`
+              }
             >
               { status }
             </span>
           </div>
           <div className="div-card-info-1-date-price">
             <div
-              data-testid={`seller_orders__element-order-date-${dataTestIdOrderId}`}
+              data-testid={ `seller_orders__element-order-date-${dataTestIdOrderId}` }
             >
               { orderDate }
             </div>
             <div
-              data-testid={`seller_orders__element-card-price-${dataTestIdOrderId}`}
+              data-testid={ `seller_orders__element-card-price-${dataTestIdOrderId}` }
             >
-              R$ { totalPrice }
+              { totalPrice }
             </div>
           </div>
         </div>
         <div
           className="div-card-info-2-address"
-          data-testid={`seller_orders__element-card-address-${dataTestIdOrderId}`}
+          data-testid={ `seller_orders__element-card-address-${dataTestIdOrderId}` }
         >
           { address }
         </div>
       </div>
     </div>
-  )
+  );
 }
+
+OrderCard.propTypes = {
+  order: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  orderDate: PropTypes.string.isRequired,
+  totalPrice: PropTypes.number.isRequired,
+  address: PropTypes.string.isRequired,
+  dataTestIdOrderId: PropTypes.string,
+};
+
+OrderCard.defaultProps = {
+  dataTestIdOrderId: '',
+};
 
 export default OrderCard;
