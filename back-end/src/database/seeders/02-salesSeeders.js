9f59718c996e5f5736caa605848ace88cebae661
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-09 18:11:20 -0300

    feat(02-salesSeeders.js): Seeder da tabela "Sales" criado e implementado

---
+++ back-end/src/database/seeders/02-salesSeeders.js
@@ -0,0 +1,50 @@
+module.exports = {
+  up: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkInsert('Sales', [
+      {
+        id: 1,
+        user_id: 3,
+        saller_id: 2,
+        total_price: 75,
+        delivery_address: 'Rua Irmãos Monteiro, Bairro Pedras, 851',
+        delivery_number: '28123456789',
+        sale_date: new Date(),
+        status: 'pendente',
+      },
+      {
+        id: 2,
+        user_id: 3,
+        saller_id: 2,
+        total_price: 184.8,
+        delivery_address: 'Rua Irmãos Monteiro, Bairro Pedras, 851',
+        delivery_number: '28123456789',
+        sale_date: new Date(),
+        status: 'preparando',
+      },
+      {
+        id: 3,
+        user_id: 3,
+        saller_id: 2,
+        total_price: 219,
+        delivery_address: 'Rua Irmãos Monteiro, Bairro Pedras, 851',
+        delivery_number: '28123456789',
+        sale_date: new Date(),
+        status: 'entregue',
+      },
+      {
+        id: 4,
+        user_id: 3,
+        saller_id: 2,
+        total_price: 1427.73,
+        delivery_address: 'Rua Irmãos Monteiro, Bairro Pedras, 851',
+        delivery_number: '28123456789',
+        sale_date: new Date(),
+        status: 'pendente',
+      },
+    ], { timestamps: false });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkDelete('Sales', null, {});
+  },
+};
