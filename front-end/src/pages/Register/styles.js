58d5ef9ed0825b584d8a3ee0732e90ccafbdb59c
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-09 16:23:30 -0300

    style(Register/styles.js): :art: Alterações feitas para corrigir erros de formatação de código.

--- front-end/src/pages/Register/styles.js
+++ front-end/src/pages/Register/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+const Container = styled.div`
   width: 100vw;
   height: 100vh;
 
@@ -33,3 +33,5 @@ export const Container = styled.div`
     justify-content: center;
   }
 `;
+
+export default Container;
