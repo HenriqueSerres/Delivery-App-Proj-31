50cf678b2f915fe1b1a692ab767b6d2d20eddfbe
caio-cmc <caiocesarmcruz@gmail.com>
2022-08-09 18:39:50 -0300

    feat(salesController.js): :sparkles: findAnUserOrder implemented

--- back-end/src/controllers/salesController.js
+++ back-end/src/controllers/salesController.js
@@ -6,6 +6,15 @@ const findOrders = async (req, res) => {
   return res.status(200).json(sales);
 };
 
+const findAnUserOrder = async (req, res) => {
+  const { id } = req.params;
+  const { data } = req.user;
+  const userOrders = await salesService.filterUserOrderbyId(id, data);
+  if (!userOrders) return res.status(404).json({ message: 'Order not found' });
+  return res.status(200).json(userOrders);
+};
+
 module.exports = {
   findOrders,
+  findAnUserOrder,
 };
