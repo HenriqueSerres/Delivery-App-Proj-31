98c21f9169598f186f63fe5476d6cd96e5b1281b
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-08-09 17:20:03 -0300

    docs: Renomeando variáveis de ContextRegister para evitar conflito

--- front-end/src/context/ContextRegister/index.js
+++ front-end/src/context/ContextRegister/index.js
@@ -3,26 +3,26 @@ import validateEmail from '../../helpers/data';
 import { MIN_LENGTH_LOGIN } from '../../helpers/constants';
 
 const ContextLogin = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [disabled, setDisabled] = useState(true);
+  const [nameRegister, setNameRegister] = useState('');
+  const [emailRegister, setEmailRegister] = useState('');
+  const [passwordRegister, setPasswordRegister] = useState('');
+  const [disabledRegister, setDisabledRegister] = useState(true);
 
   useEffect(() => {
-    const nameCheck = name.length >= 2 * MIN_LENGTH_LOGIN;
-    const emailCheck = validateEmail(email);
-    const passwordCheck = password.length >= MIN_LENGTH_LOGIN;
-    emailCheck && passwordCheck ? setDisabled(false) : setDisabled(true);
-  }, [email, password]);
+    const nameCheck = nameRegister.length >= 2 * MIN_LENGTH_LOGIN;
+    const emailCheck = validateEmail(emailRegister);
+    const passwordCheck = passwordRegister.length >= MIN_LENGTH_LOGIN;
+    emailCheck && passwordCheck ? setDisabledRegister(false) : setDisabledRegister(true);
+  }, [emailRegister, passwordRegister]);
 
   const contextRegisterObj = {
-    setName,
-    name,
-    setEmail,
-    email,
-    setPassword,
-    password,
-    disabled,
+    nameRegister,
+    setNameRegister,
+    emailRegister,
+    setEmailRegister,
+    passwordRegister,
+    setPasswordRegister,
+    disabledRegister
   };
 
   return { contextRegisterObj };
