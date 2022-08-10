800f513c280e4c8596fabe231cc3e01153df7da2
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-08-10 20:05:00 -0300

    docs(service): :art: Alterações feitas para corrigir erros de formatação de código.

--- front-end/src/services/index.js
+++ front-end/src/services/index.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import { URL_PRODUCTS } from '../helpers/constants';
 
+const applicationJSON = 'application/json';
+
 const setOptions = (url, method, data) => ({
   url,
   method,
   headers: {
     // "x-access-token": token,
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
+    Accept: applicationJSON,
+    'Content-Type': applicationJSON,
   },
   data,
 });
@@ -15,22 +17,19 @@ const setOptions = (url, method, data) => ({
 export const getAxiosRequest = async () => {
   const userData = await JSON.parse(localStorage.getItem('userData'));
   if (!userData) return;
-  const token = userData.token;
+  const { token } = userData;
   return axios
     .get(URL_PRODUCTS, {
       headers: {
         'x-access-token': token,
-        Accept: 'application/json',
-        Authorization: token
-      }
+        Accept: applicationJSON,
+        Authorization: token,
+      },
     })
     .then((response) => response.data)
     .catch((err) => err);
 };
 
-export const axiosRequest = async (url, method, data) =>
-  await axios(setOptions(url, method, data))
-    .then((response) => response)
-    .catch((err) => err);
-
-export default axiosRequest;
+export const axiosRequest = (u, m, d) => axios(setOptions(u, m, d))
+  .then((response) => response)
+  .catch((err) => err);
