073c805fb31336eaa06c85e4736678bd43295ba5
caio-cmc <caiocesarmcruz@gmail.com>
2022-08-10 16:49:22 -0300

    refactor(ssalesController.js): :zap: Try catch added on salsController.js

--- back-end/src/controllers/salesController.js
+++ back-end/src/controllers/salesController.js
@@ -1,17 +1,24 @@
 const salesService = require('../services/salesService');
 
-const findOrders = async (req, res) => {
-  const { id, role } = req.user.data;
-  const sales = await salesService.filterAllOrders(id, role);
-  return res.status(200).json(sales);
+const findOrders = async (req, res, next) => {
+  try {
+    const { id, role } = req.user.data;
+    const sales = await salesService.filterAllOrders(id, role);
+    return res.status(200).json(sales);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const findAnUserOrder = async (req, res) => {
-  const { id } = req.params;
-  const { data } = req.user;
-  const userOrders = await salesService.filterUserOrderbyId(id, data);
-  if (!userOrders) return res.status(404).json({ message: 'Order not found' });
-  return res.status(200).json(userOrders);
+const findAnUserOrder = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { data } = req.user;
+    const userOrders = await salesService.filterUserOrderbyId(id, data);
+    return res.status(200).json(userOrders);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
