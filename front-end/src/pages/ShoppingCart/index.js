96a8803acb8889f96f2be98ad244d12590e7597c
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-08-10 20:08:17 -0300

    docs(ShoppingCart): :art: Alterações feitas para corrigir erros de formatação de código.

--- front-end/src/pages/ShoppingCart/index.js
+++ front-end/src/pages/ShoppingCart/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import Cart from '../../components/ShoppingCart/Cart';
 import Context from '../../context/Context';
 
@@ -10,12 +10,18 @@ function ShoppingCart() {
   return (
     <Container>
       <h3>Finalizar Pedido</h3>
-      {shoppingCartItems !== undefined &&
-        shoppingCartItems !== null &&
-        shoppingCartItems
+      {shoppingCartItems !== undefined
+        && shoppingCartItems !== null
+        && shoppingCartItems
           .filter((item) => item.quantity > 0)
           .map(({ name, quantity, price }, id) => (
-            <Cart id={id} name={name} quantity={quantity} price={price} key={name} />
+            <Cart
+              id={ id }
+              name={ name }
+              quantity={ quantity }
+              price={ price }
+              key={ name }
+            />
           ))}
       <h2>
         Total:
