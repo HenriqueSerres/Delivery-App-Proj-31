142bda51aea1764a1ea3eeb7d4021fb4ccbc8f1d
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-10 21:51:04 -0300

    feat(pages/SellerOrders.jsx): Contexto da página "SellerOrders" implementado

--- front-end/src/pages/SellerOrders.jsx
+++ front-end/src/pages/SellerOrders.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import OrderCard from '../components/OrderCard';
-// import fetchAllOrders from '../services/fetchOrders';
+import Context from '../context/Context';
 import '../styles/sellerOrders.css';
 
 function SellerOrders() {
-  const orders = [];
-  // fetchAllOrders('http://localhost:3001/mock/orders', 'token').then((data) => {
-  //   orders = data;
-  // });
+  const { stateAllOrders } = useContext(Context);
   return (
     <section className="seller-orders-page">
       <span>Tela de pedidos do Vendedor</span>
@@ -15,7 +12,7 @@ function SellerOrders() {
       <br />
       <section className="section-order-cards">
         {
-          orders.map(({ id, status, saleDate, totalPrice, deliveryAddress }) => (
+          stateAllOrders.map(({ id, status, saleDate, totalPrice, deliveryAddress }) => (
             <OrderCard
               key={ id }
               order={ `Pedido ${id}` }
