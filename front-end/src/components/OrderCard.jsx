de9ada98b1a7369d083487030ddb3671baf7d7e3
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-11 18:25:32 -0300

    refactor(components/OrderCard.jsx): Alterações feitas para tornar os "data-testid's" do componente dinâmicos

--- front-end/src/components/OrderCard.jsx
+++ front-end/src/components/OrderCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { formatOrderNumber } from '../helpers/formatNumber';
+import { useHistory } from 'react-router-dom';
 
 function OrderCard({
   orderId,
@@ -8,7 +9,11 @@ function OrderCard({
   orderDate,
   totalPrice,
   address,
+  pathRoute,
+  userRole,
 }) {
+  const history = useHistory();
+  const userRoleFormatted = userRole.toLowerCase();
   const styleOrderStatus = (orderStatus) => {
     let result = {};
     switch (orderStatus) {
@@ -35,46 +40,46 @@ function OrderCard({
     return result;
   }
   return (
-    <div className="div-card">
-      <div className="div-card-order">
-        <span
-          className="span-card-order"
-          data-testid={ `seller_orders__element-order-id-${orderId}` }
-        >
-          Pedido { formatOrderNumber(orderId) }
-        </span>
-      </div>
-      <div className="div-card-info">
-        <div className="div-card-info-1">
-          <div className="div-card-info-1-order-status" style={ styleOrderStatus(status) }>
-            <span
-              data-testid={
-                `seller_orders__element-delivery-status-${orderId}`
-              }
-            >
-              { status.toUpperCase() }
-            </span>
+    <div className="div-card" onClick={ () => history.push(`${pathRoute}/${orderId}`)}>
+        <div className="div-card-order">
+          <span
+            className="span-card-order"
+            data-testid={ `${userRoleFormatted}_orders__element-order-id-${orderId}` }
+          >
+            Pedido { formatOrderNumber(orderId) }
+          </span>
+        </div>
+        <div className="div-card-info">
+          <div className="div-card-info-1">
+            <div className="div-card-info-1-order-status" style={ styleOrderStatus(status) }>
+              <span
+                data-testid={
+                  `${userRoleFormatted}_orders__element-delivery-status-${orderId}`
+                }
+              >
+                { status.toUpperCase() }
+              </span>
+            </div>
+            <div className="div-card-info-1-date-price">
+              <div
+                data-testid={ `${userRoleFormatted}_orders__element-order-date-${orderId}` }
+              >
+                { orderDate }
+              </div>
+              <div
+                data-testid={ `${userRoleFormatted}_orders__element-card-price-${orderId}` }
+              >
+                { totalPrice.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'}) }
+              </div>
+            </div>
           </div>
-          <div className="div-card-info-1-date-price">
-            <div
-              data-testid={ `seller_orders__element-order-date-${orderId}` }
-            >
-              { orderDate }
-            </div>
-            <div
-              data-testid={ `seller_orders__element-card-price-${orderId}` }
-            >
-              { totalPrice.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'}) }
-            </div>
+          <div
+            className="div-card-info-2-address"
+            data-testid={ `${userRoleFormatted}_orders__element-card-address-${orderId}` }
+          >
+            { address }
           </div>
         </div>
-        <div
-          className="div-card-info-2-address"
-          data-testid={ `seller_orders__element-card-address-${orderId}` }
-        >
-          { address }
-        </div>
-      </div>
     </div>
   );
 }
