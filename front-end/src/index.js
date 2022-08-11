e8c0b3fe0a850139e127b022ef22d23c4f17df2d
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-10 21:52:11 -0300

    fix(front-end/src/index.js): :bug: Importação duplicada do "BrowserRouter" removida

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
