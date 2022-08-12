f7a7ae4c4d591e05809dc1094410f5a4b215da0a
caio-cmc <caiocesarmcruz@gmail.com>
2022-08-12 15:42:33 -0300

    refactor(routes/admin/index.js): :art: post endpoint now uses adminController

--- back-end/src/routes/adm/index.js
+++ back-end/src/routes/adm/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const adminController = require('../../controllers/adminController');
-const registerController = require('../../controllers/registerController');
 const adminCheck = require('../../middlewares/adminCheck');
 const registerIsValid = require('../../middlewares/registerIsValid');
 const tokenIsValid = require('../../middlewares/tokenIsValid');
+const userAlreadyExists = require('../../middlewares/userAlreadyExists');
 
 const adminRoutes = express.Router();
 
-adminRoutes.post('/', tokenIsValid, adminCheck, registerIsValid, registerController.register);
+adminRoutes.post('/',
+  tokenIsValid,
+  adminCheck,
+  registerIsValid,
+  userAlreadyExists,
+  adminController.registerUserAdm);
 adminRoutes.get('/', tokenIsValid, adminCheck, adminController.getAllUsers);
 adminRoutes.delete('/:id', tokenIsValid, adminCheck, adminController.removeUser);
 
