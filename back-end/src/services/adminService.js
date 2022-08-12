7e15ff83244b27be33a999db51aefb0e4f0db75e
caio-cmc <caiocesarmcruz@gmail.com>
2022-08-12 15:44:27 -0300

    feat(adminService.js): :sparkles: registerUserAdm function implemented

--- back-end/src/services/adminService.js
+++ back-end/src/services/adminService.js
@@ -1,5 +1,22 @@
+const cryptoJs = require('crypto-js');
 const { User } = require('../database/models');
 const handleError = require('../utils/handleError');
+const generateJWT = require('../utils/generateToken');
+const { JWT_SUPER_SECRET } = require('../constants');
+
+const registerUserAdm = async (body) => {
+  const token = generateJWT('1d', body, JWT_SUPER_SECRET);
+  const { name, email, password, role } = body;
+  const encryptedPass = cryptoJs.MD5(password).toString();
+  const newUser = await User.create({ name, email, password: encryptedPass, role });
+  const user = newUser.dataValues;
+  const filteredResponse = { id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    token };
+  return filteredResponse;
+};
 
 const getAllUsers = async () => {
   const allUsers = await User.findAll({
@@ -18,4 +35,5 @@ const removeUser = async (id) => {
 module.exports = {
   getAllUsers,
   removeUser,
+  registerUserAdm,
 };
