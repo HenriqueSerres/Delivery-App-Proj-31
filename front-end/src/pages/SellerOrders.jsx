aaa295ec4fed05ecbf082b063ab2cacb1b0042aa
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-15 17:47:36 -0300

    style(src/pages/SellerOrders.jsx): :art: Alterações feitas para corrigir erros de formatação de código

--- front-end/src/pages/SellerOrders.jsx
+++ front-end/src/pages/SellerOrders.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import OrderCard from '../components/OrderCard';
 import Context from '../context/Context';
 import '../styles/sellerOrders.css';
@@ -8,42 +9,52 @@ function SellerOrders({ match: { path } }) {
   const { stateAllOrders, stateDisplayPage } = useContext(Context);
   return (
     <section>
-      {
-        stateDisplayPage
-        ? (<section className="seller-orders-page">
-            <Header />
-            <br />
-            <br />
-            <section className="section-order-cards">
-              {
-                stateAllOrders.map(({ id, status, saleDate, totalPrice, deliveryAddress }) => (
-                  <OrderCard
-                    key={ id }
-                    orderId={ id }
-                    status={ status }
-                    orderDate={ saleDate }
-                    totalPrice={ Number(totalPrice) }
-                    address={ deliveryAddress }
-                    pathRoute={ path }
-                    userRole="seller"
-                  />
-                ))
-              }
-            </section>
-            <br />
-            <br />
-          </section>)
-        : (
-            <section>
-              <span>A página parece estar indiponível !</span>
-              <br />
-              <span>DESCULPE :\</span>
-            </section>
-          )
-      }
+      {stateDisplayPage ? (
+        <section className="seller-orders-page">
+          <Header />
+          <br />
+          <br />
+          <section className="section-order-cards">
+            { stateAllOrders.map(
+              ({ id, status, saleDate, totalPrice, deliveryAddress }) => (
+                <OrderCard
+                  key={ id }
+                  orderId={ id }
+                  status={ status }
+                  orderDate={ saleDate }
+                  totalPrice={ Number(totalPrice) }
+                  address={ deliveryAddress }
+                  pathRoute={ path }
+                  userRole="seller"
+                />
+              ),
+            ) }
+          </section>
+          <br />
+          <br />
+        </section>
+      ) : (
+        <section>
+          <span>A página parece estar indiponível !</span>
+          <br />
+          <span>DESCULPE :\</span>
+        </section>
+      )}
     </section>
-    
   );
 }
+
+SellerOrders.propTypes = {
+  match: PropTypes.shape({
+    isExact: PropTypes.bool,
+    params: PropTypes.objectOf(PropTypes.string),
+    path: PropTypes.string,
+  }),
+  path: PropTypes.string.isRequired,
+};
+
+SellerOrders.defaultProps = {
+  match: {},
+};
 
 export default SellerOrders;
