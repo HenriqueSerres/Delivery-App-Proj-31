47ac52fee3e62b5e2f267dbcc6daa64de0906052
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-15 17:45:35 -0300

    style(components/OrderCard.jsx): :art: Alterações feitas para corrigir erros de formatação de código

--- front-end/src/components/OrderCard.jsx
+++ front-end/src/components/OrderCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { formatOrderNumber } from '../helpers/formatNumber';
 import { useHistory } from 'react-router-dom';
+import { formatOrderNumber } from '../helpers/formatNumber';
 
 function OrderCard({
   orderId,
@@ -17,70 +17,80 @@ function OrderCard({
   const styleOrderStatus = (orderStatus) => {
     let result = {};
     switch (orderStatus) {
-      case 'pendente':
-        result = {
-          backgroundColor: 'rgb(211, 188, 60)',
-          padding: '35px 45px',
-        };
-        break;
-      case 'preparando':
-        result = { backgroundColor: 'rgb(89, 184, 51)' };
-        break;
-      case 'entregue':
-        result = {
-          backgroundColor: 'rgb(60, 211, 166)',
-          padding: '35px 45px',
-        };
-        break;
+    case 'pendente':
+      result = {
+        backgroundColor: 'rgb(211, 188, 60)',
+        padding: '35px 45px',
+      };
+      break;
+    case 'preparando':
+      result = { backgroundColor: 'rgb(89, 184, 51)' };
+      break;
+    case 'entregue':
+      result = {
+        backgroundColor: 'rgb(60, 211, 166)',
+        padding: '35px 45px',
+      };
+      break;
 
-      default:
-        result = { backgroundColor: 'rgb(150, 150, 150)' };
-        break;
+    default:
+      result = { backgroundColor: 'rgb(150, 150, 150)' };
+      break;
     }
     return result;
-  }
+  };
   return (
-    <div className="div-card" onClick={ () => history.push(`${pathRoute}/${orderId}`)}>
-        <div className="div-card-order">
-          <span
-            className="span-card-order"
-            data-testid={ `${userRoleFormatted}_orders__element-order-id-${orderId}` }
-          >
-            Pedido { formatOrderNumber(orderId) }
-          </span>
-        </div>
-        <div className="div-card-info">
-          <div className="div-card-info-1">
-            <div className="div-card-info-1-order-status" style={ styleOrderStatus(status) }>
-              <span
-                data-testid={
-                  `${userRoleFormatted}_orders__element-delivery-status-${orderId}`
-                }
-              >
-                { status.toUpperCase() }
-              </span>
-            </div>
-            <div className="div-card-info-1-date-price">
-              <div
-                data-testid={ `${userRoleFormatted}_orders__element-order-date-${orderId}` }
-              >
-                { orderDate }
-              </div>
-              <div
-                data-testid={ `${userRoleFormatted}_orders__element-card-price-${orderId}` }
-              >
-                { totalPrice.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'}) }
-              </div>
-            </div>
-          </div>
+    <button
+      className="div-card"
+      type="button"
+      onClick={ () => history.push(`${pathRoute}/${orderId}`) }
+    >
+      <div className="div-card-order">
+        <span
+          className="span-card-order"
+          data-testid={ `${userRoleFormatted}_orders__element-order-id-${orderId}` }
+        >
+          Pedido
+          { formatOrderNumber(orderId) }
+        </span>
+      </div>
+      <div className="div-card-info">
+        <div className="div-card-info-1">
           <div
-            className="div-card-info-2-address"
-            data-testid={ `${userRoleFormatted}_orders__element-card-address-${orderId}` }
+            className="div-card-info-1-order-status"
+            style={ styleOrderStatus(status) }
           >
-            { address }
+            <span
+              data-testid={
+                `${userRoleFormatted}_orders__element-delivery-status-${orderId}`
+              }
+            >
+              { status.toUpperCase() }
+            </span>
+          </div>
+          <div className="div-card-info-1-date-price">
+            <div
+              data-testid={ `${userRoleFormatted}_orders__element-order-date-${orderId}` }
+            >
+              { orderDate }
+            </div>
+            <div
+              data-testid={ `${userRoleFormatted}_orders__element-card-price-${orderId}` }
+            >
+              {
+                totalPrice.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+              }
+            </div>
           </div>
         </div>
-    </div>
+        <div
+          className="div-card-info-2-address"
+          data-testid={ `${userRoleFormatted}_orders__element-card-address-${orderId}` }
+        >
+          { address }
+        </div>
+      </div>
+    </button>
   );
 }
 
@@ -89,7 +99,13 @@ OrderCard.propTypes = {
   status: PropTypes.string.isRequired,
   orderDate: PropTypes.string.isRequired,
   totalPrice: PropTypes.number.isRequired,
-  address: PropTypes.string.isRequired,
+  address: PropTypes.string,
+  pathRoute: PropTypes.string.isRequired,
+  userRole: PropTypes.string.isRequired,
+};
+
+OrderCard.defaultProps = {
+  address: '',
 };
 
 export default OrderCard;
