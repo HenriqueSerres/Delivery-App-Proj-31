9544003ea5b1e5c066d197d92d2cb666db8b1d22
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-15 17:05:37 -0300

    refactor(context/ContextSellerOrders/index.js): Alterações feitas para implementar lógica de tratamento de respostas negativas do back-end

--- front-end/src/context/ContextSellerOrders/index.js
+++ front-end/src/context/ContextSellerOrders/index.js
@@ -1,30 +1,43 @@
 import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import formatDate from '../../helpers/formatDate';
 import fetchAllOrders from '../../services/fetchOrders';
 
 function ContextSellerOrders() {
   const [stateAllOrders, setStateAllOrders] = useState([]);
+  const [stateDisplayPage, setStateDisplayPage] = useState(false);
+  const history = useHistory();
 
   useEffect(() => {
-    const userToken = JSON.parse(localStorage.getItem('userData'));
+    const userToken = JSON.parse(localStorage.getItem('user'));
     if (userToken) {
       fetchAllOrders('http://localhost:3001/orders', userToken.token)
       .then((data) => {
-        const dataFormatted = data.map((elementObj) =>  {
-          const order = elementObj;
-          const saleDate = new Date(order.saleDate);
-          order.saleDate = formatDate(saleDate);
-          return order;
-        })
-        setStateAllOrders(dataFormatted);
+        if (Array.isArray(data) && !Object.keys(data).includes('message')) {
+          const dataFormatted = data.map((elementObj) =>  {
+            const order = elementObj;
+            const saleDate = new Date(order.saleDate);
+            order.saleDate = formatDate(saleDate);
+            return order;
+          });
+          setStateAllOrders(dataFormatted);
+          setStateDisplayPage(true);
+        } else {
+          if (data.httpStatusCode === 401) history.push('/login');
+          return [];
+        }
       })
-      .catch((error) => console.error(error.message));
+      .catch((error) => console.error(error));
+    } else {
+      history.push('/login');
     }
   }, []);
 
   const contextSellerOrdersObj = {
     stateAllOrders,
     setStateAllOrders,
+    stateDisplayPage,
+    setStateDisplayPage,
   }
 
   return { contextSellerOrdersObj };
