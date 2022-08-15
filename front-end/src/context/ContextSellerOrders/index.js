4f5b2e260f00cc2d16dcbe6dca43872e7f81f3d9
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-15 17:46:33 -0300

    style(context/ContextSellerOrders/index.js): :art: Alterações feitas para corrigir erros de formatação de código

--- front-end/src/context/ContextSellerOrders/index.js
+++ front-end/src/context/ContextSellerOrders/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { HTTP_UNAUTHORIZED } from '../../helpers/constants';
 import formatDate from '../../helpers/formatDate';
 import fetchAllOrders from '../../services/fetchOrders';
 
@@ -11,23 +12,22 @@ function ContextSellerOrders() {
   useEffect(() => {
     const userToken = JSON.parse(localStorage.getItem('user'));
     if (userToken) {
-      fetchAllOrders('http://localhost:3001/orders', userToken.token)
-      .then((data) => {
-        if (Array.isArray(data) && !Object.keys(data).includes('message')) {
-          const dataFormatted = data.map((elementObj) =>  {
-            const order = elementObj;
-            const saleDate = new Date(order.saleDate);
-            order.saleDate = formatDate(saleDate);
-            return order;
-          });
-          setStateAllOrders(dataFormatted);
-          setStateDisplayPage(true);
-        } else {
-          if (data.httpStatusCode === 401) history.push('/login');
-          return [];
-        }
-      })
-      .catch((error) => console.error(error));
+      fetchAllOrders('http://localhost:3001/orders', '')
+        .then((data) => {
+          if (Array.isArray(data) && !Object.keys(data).includes('message')) {
+            const dataFormatted = data.map((elementObj) => {
+              const order = elementObj;
+              const saleDate = new Date(order.saleDate);
+              order.saleDate = formatDate(saleDate);
+              return order;
+            });
+            setStateAllOrders(dataFormatted);
+            setStateDisplayPage(true);
+          } else {
+            if (data.httpStatusCode === HTTP_UNAUTHORIZED) history.push('/login');
+            return [];
+          }
+        }).catch((error) => console.error(error));
     } else {
       history.push('/login');
     }
@@ -38,7 +38,7 @@ function ContextSellerOrders() {
     setStateAllOrders,
     stateDisplayPage,
     setStateDisplayPage,
-  }
+  };
 
   return { contextSellerOrdersObj };
 }
