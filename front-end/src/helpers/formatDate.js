4d54d905007d89eace55d60b31242cdab6fb8af0
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-15 17:47:04 -0300

    style(src/helpers/formatDate.js): :art: Alterações feitas para corrigir erros de formatação de código

--- front-end/src/helpers/formatDate.js
+++ front-end/src/helpers/formatDate.js
@@ -1,7 +1,7 @@
 import { formatNumber } from './formatNumber';
 
 const formatDate = (date) => {
-  if(!(date instanceof Date)) return 'is not a date';
+  if (!(date instanceof Date)) return 'is not a date';
   const day = formatNumber(date.getDay());
   const month = formatNumber(date.getMonth());
   const year = formatNumber(date.getFullYear());
