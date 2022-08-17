760f7129213c901c61f4af9365c18626b4a117cd
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-17 17:37:56 -0300

    refactor(utils/generateToken.js): Alterações feitas para implementar nova lógica de extração/obtenção da senha mestre do JWT

--- back-end/src/utils/generateToken.js
+++ back-end/src/utils/generateToken.js
@@ -1,12 +1,14 @@
 const jwt = require('jsonwebtoken');
+const extractToken = require('./extractToken');
 require('dotenv').config();
 
-const generateJWT = (validity, paylod, superSecret) => {
+const generateJWT = (validity, paylod) => {
+  const jwtSuperSecret = extractToken(`${__dirname}/../../jwt.evaluation.key`);
   const jwtConfig = {
     expiresIn: validity,
     algorithm: 'HS256',
   };
-  return jwt.sign({ data: paylod }, superSecret, jwtConfig);
+  return jwt.sign({ data: paylod }, jwtSuperSecret, jwtConfig);
 };
 
 module.exports = generateJWT;
