148a6e8665e0ffa1b927177f004345bd28f7ca29
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-20 20:09:09 -0300

    feat(helpers/constants.js): Constantes "HTTP_BADREQUEST" e "HTTP_CONFLICT" adicionadas ao arquivo centralizador de constantes

--- front-end/src/helpers/constants.js
+++ front-end/src/helpers/constants.js
@@ -14,4 +14,8 @@ export const URL_ORDERS = 'http://localhost:3001/orders';
 
 export const HTTP_UNAUTHORIZED = 401;
 
+export const HTTP_BADREQUEST = 400;
+
+export const HTTP_CONFLICT = 409;
+
 export const HTTP_OK = 200;
