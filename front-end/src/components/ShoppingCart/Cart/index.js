d1401e4a4bddbad9ec82c6c92f10a20c16218509
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-20 19:41:14 -0300

    refactor(ShoppingCart/Cart/index.js): Alterações feitas para alinhar código com as novas correções de bug's feitas em códigos anteriores

--- front-end/src/components/ShoppingCart/Cart/index.js
+++ front-end/src/components/ShoppingCart/Cart/index.js
@@ -1,57 +1,39 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import Context from '../../../context/Context';
 
-function Cart({ id, name, quantity, price }) {
-  const { shoppingCartItems, setTotal,
-    calculateTotalPrice, setShoppingCartItems } = useContext(Context);
-
-  const handleClick = () => {
-    const filterByName = shoppingCartItems.map((item) => {
-      if (item.name === name) {
-        item.quantity = 0;
-      }
-      return item;
-    });
-    setShoppingCartItems(filterByName);
-    const calculateTotal = calculateTotalPrice(shoppingCartItems);
-    setTotal(calculateTotal);
-    localStorage.setItem('carrinho', JSON.stringify(filterByName));
-  };
-
+function Cart({ itemId, index, name, quantity, price, funcRemoveProduct }) {
   return (
-
     <>
       <tr>
         <td
           data-testid={
-            `customer_checkout__element-order-table-item-number--${id}`
+            `customer_checkout__element-order-table-item-number--${index}`
           }
         >
-          {id + 1}
+          {index + 1}
         </td>
-        <td data-testid={ `customer_checkout__element-order-table-name--${id}` }>
+        <td data-testid={ `customer_checkout__element-order-table-name--${index}` }>
           {name}
         </td>
-        <td data-testid={ `customer_checkout__element-order-table-quantity--${id}` }>
+        <td data-testid={ `customer_checkout__element-order-table-quantity--${index}` }>
           {quantity}
         </td>
         <td
           data-testid={
-            `customer_checkout__element-order-table-unit-price--${id}`
+            `customer_checkout__element-order-table-unit-price--${index}`
           }
         >
           {Number(price).toFixed(2).toString().replace('.', ',')}
         </td>
-        <td data-testid={ `customer_checkout__element-order-table-sub-total--${id}` }>
+        <td data-testid={ `customer_checkout__element-order-table-sub-total--${index}` }>
           {(quantity * price).toFixed(2).toString().replace('.', ',')}
         </td>
       </tr>
 
       <button
         type="button"
-        onClick={ () => handleClick() }
-        data-testid={ `customer_checkout__element-order-table-remove--${id}` }
+        onClick={ () => funcRemoveProduct(itemId) }
+        data-testid={ `customer_checkout__element-order-table-remove--${index}` }
       >
         Remover
       </button>
@@ -60,7 +42,7 @@ function Cart({ id, name, quantity, price }) {
 }
 
 Cart.propTypes = {
-  id: PropTypes.string,
+  index: PropTypes.string,
   name: PropTypes.string,
   quantity: PropTypes.number,
   price: PropTypes.number,
